e030959c1ecab9e14c212ac374d7b15d09e2bb9c
Ubuntu <ubuntu@ip-172-31-6-118.ap-southeast-2.compute.internal>
2020-10-14 11:03:00 +0000

    rename deploy script

--- 2020-10/app/deploy_lambda_package.sh
+++ 2020-10/app/deploy_lambda_package.sh
@@ -4,7 +4,7 @@ rm -f CovidDataFunction.zip
 
 virtualenv v-env
 source v-env/bin/activate
-pip3 install --prefix= --target ./package -r requirements.txt
+pip3 install --prefix= -r requirements.txt
 deactivate
 
 cd v-env/lib/python3.6/site-packages
@@ -13,7 +13,3 @@ cd $OLDPWD
 zip -g CovidDataFunction.zip *.py
 
 aws s3 cp CovidDataFunction.zip s3://$(aws cloudformation describe-stacks --stack-name core-infrastructure-function-bucket --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
-
-rm -rf package
-rm -rf v-env
-rm -f CovidDataFunction.zip
