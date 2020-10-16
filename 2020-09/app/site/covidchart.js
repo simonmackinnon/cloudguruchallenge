1d285a85ba216f111e356ea06c06ea34df1bc54a
Ubuntu <ubuntu@ip-172-31-6-118.ap-southeast-2.compute.internal>
2020-10-16 03:40:33 +0000

    removed firehose/glue stuff, add infra to put api gateway in front on dynamo, add simple site for demo

---
+++ 2020-09/app/site/covidchart.js
@@ -0,0 +1,115 @@
+function getColumn(arr, n) {
+    return (arr, n) => arr.map(x => x[n])
+}
+
+function compare( a, b ) {
+  if ( a.date < b.date ){
+    return -1;
+  }
+  if ( a.date > b.date ){
+    return 1;
+  }
+  return 0;
+}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+  
+fetch("https://7vmx7x6mwg.execute-api.ap-southeast-2.amazonaws.com/live/cumulativedata", {
+  "headers": {},
+  "referrer": "",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+})
+.then((resp) => resp.json())
+.then(function displayChart(chartData) {
+	
+	const colors = {
+	  green: {
+	    fill: '#e0eadf',
+	    stroke: '#5eb84d',
+	  },
+	  lightBlue: {
+	    stroke: '#6fccdd',
+	  },
+	  darkBlue: {
+	    fill: '#92bed2',
+	    stroke: '#3282bf',
+	  },
+	  purple: {
+	    fill: '#8fa8c8',
+	    stroke: '#75539e',
+	  },
+	};
+	
+	const cases = [];
+	const deaths = [];
+	const recovered = [];
+	const date = [];
+	
+	var records = chartData['records'];
+	
+	records.sort( compare );
+	
+	for(var i=0; i<records.length; i++){
+	  date.push(records[i]['date']);
+	  deaths.push(records[i]['deaths']);
+	  recovered.push(records[i]['recovered']);
+	  cases.push(records[i]['cases']);
+	}
+	
+	const myChart = new Chart(ctx, {
+	  type: 'line',
+	  data: {
+	 
+	    labels: date,
+	    datasets: [{
+	      label: "Recovered",
+	      fill: true,
+	      backgroundColor: colors.purple.fill,
+	      pointBackgroundColor: colors.purple.stroke,
+	      borderColor: colors.purple.stroke,
+	      pointHighlightStroke: colors.purple.stroke,
+	      borderCapStyle: 'butt',
+	      data: recovered,
+	
+	    }, {
+	      label: "Deaths",
+	      fill: true,
+	      backgroundColor: colors.darkBlue.fill,
+	      pointBackgroundColor: colors.darkBlue.stroke,
+	      borderColor: colors.darkBlue.stroke,
+	      pointHighlightStroke: colors.darkBlue.stroke,
+	      borderCapStyle: 'butt',
+	      data: deaths,
+	    }, {
+	      label: "Cases",
+	      fill: true,
+	      backgroundColor: colors.green.fill,
+	      pointBackgroundColor: colors.lightBlue.stroke,
+	      borderColor: colors.lightBlue.stroke,
+	      pointHighlightStroke: colors.lightBlue.stroke,
+	      borderCapStyle: 'butt',
+	      data: cases,
+	    }]
+	  },
+	  options: {
+	    responsive: false,
+	    // Can't just just `stacked: true` like the docs say
+	    scales: {
+	      yAxes: [{
+	        stacked: true,
+	      }]
+	    },
+	    animation: {
+	      duration: 750,
+	    },
+	    title: {
+            display: true,
+            text: 'USA COVID-19 Data (Cumulative)'
+        }
+	  }
+	});
+});
